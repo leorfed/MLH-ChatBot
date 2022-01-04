ae7f96ef73ce2c74d2dfc3b30de6b86322eb4970
CodeJedi <34801810+Darsboy@users.noreply.github.com>
2022-01-03 22:28:31 -0500

    Public

--- training.py
+++ training.py
@@ -3,7 +3,6 @@ import json
 import pickle
 import numpy as np
 
-
 import nltk
 from nltk.stem import WordNetLemmatizer
 
@@ -11,28 +10,31 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
+
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
+
+intents = json.loads(open("intents.json").read())
+
 words = []
 classes = []
 documents = []
-ignore_letters = ['?','!','.',',']
+ignore_letters = ["?", "!", ".", ","]
 
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
+for intent in intents["intents"]:
+    for pattern in intent["patterns"]:
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
-        documents.append((word_list,intent['tag']))
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
+        documents.append((word_list, intent["tag"]))
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
 
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 classes = sorted(set(classes))
 
-pickle.dump(words, open('words.pkl','wb'))
-pickle.dump(classes, open('classes.pkl','wb'))
+pickle.dump(words, open("words.pkl", "wb"))
+pickle.dump(classes, open("classes.pkl", "wb"))
 
 training = []
 output_empty = [0] * len(classes)
